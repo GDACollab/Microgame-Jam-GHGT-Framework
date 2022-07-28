1c48e3ed3f69fe8385492c0515417be0c8d9bf80
ambiguousname <tyly04@gmail.com>
2022-07-28 11:11:03 -0700

    Updated private variables to reflect that they are private

--- Extensions/Unity/MicrogameJamController.cs
+++ Extensions/Unity/MicrogameJamController.cs
@@ -28,62 +28,62 @@ public class MicrogameJamController : MonoBehaviour {
     [DllImport("__Internal")]
     private static extern void SetTimerMax(float time);
 
-    private float time;
+    private float _time;
 
-    private float maxTime = 20.0f;
+    private float _maxTime = 20.0f;
 
-    private bool timeChangesLocked = false;
+    private bool _timeChangesLocked = false;
 
-    private bool isInGame = false;
+    private bool _isInGame = false;
 
     // For dev mode:
-    private Scene startScene;
+    private Scene _startScene;
 
     // This way, you can call SetMaxTimer in the start function BEFORE gameStart is called.
     private void Start(){
         DontDestroyOnLoad(this.gameObject);
         #if UNITY_WEBGL && !UNITY_EDITOR
-            isInGame = GameExists();
+            _isInGame = GameExists();
         #else
             // isInGame is false by default.
-            time = 0.0f;
+            _time = 0.0f;
         #endif
         StartCoroutine(GameStartDelay());
     }
 
     IEnumerator GameStartDelay() {
         yield return new WaitForEndOfFrame();
-        timeChangesLocked = true;
-        if (isInGame) {
+        _timeChangesLocked = true;
+        if (_isInGame) {
             StartGame();
         } else {
-            startScene = SceneManager.GetActiveScene();
+            _startScene = SceneManager.GetActiveScene();
         }
     }
 
     public void WinGame(){
-        if (isInGame){
+        if (_isInGame){
             Win();
         } else {
             // Synchronous, because what else is there to do?
             Debug.Log("Game won. Reloading...");
-            SceneManager.LoadScene(startScene.buildIndex);
-            time = 0.0f;
+            SceneManager.LoadScene(_startScene.buildIndex);
+            _time = 0.0f;
         }
     }
 
     public void LoseGame(){
-        if (isInGame){
+        if (_isInGame){
             Lose();
         } else {
             Debug.Log("Game lost. Reloading...");
-            SceneManager.LoadScene(startScene.buildIndex);
-            time = 0.0f;
+            SceneManager.LoadScene(_startScene.buildIndex);
+            _time = 0.0f;
         }
     }
 
     public int GetDifficulty() {
-        if (isInGame) {
+        if (_isInGame) {
             return Difficulty();
         } else {
             return defaultDifficulty;
@@ -91,7 +91,7 @@ public class MicrogameJamController : MonoBehaviour {
     }
 
     public int GetLives(){
-        if (isInGame){
+        if (_isInGame){
             return Lives();
         } else {
             return 3;
@@ -99,33 +99,33 @@ public class MicrogameJamController : MonoBehaviour {
     }
 
     public float GetTimer(){
-        if (isInGame){
+        if (_isInGame){
             return Timer();
         } else {
-            return time;
+            return _time;
         }
     }
 
     public bool IsInWebGL(){
-        return isInGame;
+        return _isInGame;
     }
 
-    public void SetMaxTimer(float time){
-        if (!timeChangesLocked){
-            if (isInGame) {
-                SetTimerMax(time);
+    public void SetMaxTimer(float seconds){
+        if (!_timeChangesLocked){
+            if (_isInGame) {
+                SetTimerMax(seconds);
             } else {
-                maxTime = time;
+                _maxTime = seconds;
             }
         } else {
-            Debug.LogError("SetMaxTimer(" + time + " seconds) call failed. Called AFTER Start(). Any changes to the maximum amount of allowed time must be made during the Start function in MonoBehaviour.");
+            Debug.LogError("SetMaxTimer(" + seconds + " seconds) call failed. Called AFTER Start(). Any changes to the maximum amount of allowed time must be made during the Start function in MonoBehaviour.");
         }
     }
 
     private void Update(){
-        if(!isInGame){
-            time += Time.deltaTime;
-            if (time > maxTime){
+        if(!_isInGame){
+            _time += Time.deltaTime;
+            if (_time > _maxTime){
                 Debug.Log("Reached time limit!");
                 LoseGame();
             }
