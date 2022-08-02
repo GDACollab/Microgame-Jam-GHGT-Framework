93dd2f6d48f082b0bce4a43560678383ec9cee8d
ambiguousname <tyly04@gmail.com>
2022-08-01 21:35:54 -0700

    Update gameloader.js

--- Web/gameloader.js
+++ Web/gameloader.js
@@ -1,19 +1,19 @@
 // Add games to be loaded here:
 var gamesList = {
-    //"GimmeFive": "index.html",
+    "GimmeFive": "index.html",
     "Pico8 MGG22 Test": "mgg_test.html"
 };
 
+// TODO: Make game picking more robust, add difficulty increases, etc.
 function loadGame(){
     let gameToLoad = Object.keys(gamesList)[Math.floor(Math.random() * Object.keys(gamesList).length)];
     let gameURL = "./games/" + gameToLoad + "/" + gamesList[gameToLoad];
     document.getElementById("game").src = gameURL;
-
-    // Create an element to show over the iframe:
-    
 }
 
 function iframeLoaded(){
+    // Focus the iframe so inputs get through:
+    document.getElementById("game").click();
     if (PicoInterface.isPicoRunning()){
         var picoInterface = new PicoInterface();
         picoInterface.interfaceWithPico();
