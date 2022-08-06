ea7e099098a61d991d9bb29e39ed52f7763b1d35
ambiguousname <tyly04@gmail.com>
2022-08-06 09:49:33 -0700

    Fixing MicrogameJamController function calls for unexpected values

--- Extensions/Unity/MicrogameJamController.cs
+++ Extensions/Unity/MicrogameJamController.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class MicrogameJamController : MonoBehaviour {
+    const float MIN_TIME_ALLOWED = 5;
+    const float MAX_TIME_ALLOWED = 15;
 
     [Range(1, 3)]
     public int defaultDifficulty = 1;
@@ -31,11 +33,13 @@ public class MicrogameJamController : MonoBehaviour {
 
     private float _time;
 
-    private float _maxTime = 20.0f;
+    private float _maxTime = MAX_TIME_ALLOWED;
 
     private bool _timeChangesLocked = false;
 
     private bool _isInGame = false;
+
+    private bool _endGameLocked = false;
 
     // For dev mode:
     private Scene _startScene;
@@ -63,23 +67,37 @@ public class MicrogameJamController : MonoBehaviour {
     }
 
     public void WinGame(){
-        if (_isInGame){
-            Win();
+        if (!_endGameLocked){
+            if (_isInGame){
+                Win();
+            } else {
+                // Synchronous, because what else is there to do?
+                Debug.Log("Game won. Reloading...");
+                SceneManager.LoadScene(_startScene.buildIndex);
+                _endGameLocked = false;
+                _timeChangesLocked = false;
+                _time = 0.0f;
+            }
+            _endGameLocked = true;
         } else {
-            // Synchronous, because what else is there to do?
-            Debug.Log("Game won. Reloading...");
-            SceneManager.LoadScene(_startScene.buildIndex);
-            _time = 0.0f;
+            Debug.LogWarning("Something tried to call LoseGame() after game has already ended.");
         }
     }
 
     public void LoseGame(){
-        if (_isInGame){
-            Lose();
+        if (!_endGameLocked){
+            if (_isInGame){
+                Lose();
+            } else {
+                Debug.Log("Game lost. Reloading...");
+                SceneManager.LoadScene(_startScene.buildIndex);
+                _endGameLocked = false;
+                _timeChangesLocked = false;
+                _time = 0.0f;
+            }
+            _endGameLocked = true;
         } else {
-            Debug.Log("Game lost. Reloading...");
-            SceneManager.LoadScene(_startScene.buildIndex);
-            _time = 0.0f;
+            Debug.LogWarning("Something tried to call LoseGame() after game has already ended.");
         }
     }
 
@@ -109,18 +127,25 @@ public class MicrogameJamController : MonoBehaviour {
 
     public void SetMaxTimer(float seconds){
         if (!_timeChangesLocked){
+            if (seconds > MAX_TIME_ALLOWED){
+                Debug.LogWarning("SetMaxTimer() of " + seconds + "s > " + MAX_TIME_ALLOWED + "s. Max timer set to " + MAX_TIME_ALLOWED + "s.");
+                seconds = MAX_TIME_ALLOWED;
+            } else if (seconds < MIN_TIME_ALLOWED) {
+                Debug.LogWarning("SetMaxTimer() of " + seconds +"s < " + MIN_TIME_ALLOWED + "s. Max timer set to " + MIN_TIME_ALLOWED + "s.");
+                seconds = MIN_TIME_ALLOWED;
+            }
             if (_isInGame) {
                 SetTimerMax(seconds);
             } else {
                 _maxTime = seconds;
             }
         } else {
-            Debug.LogError("SetMaxTimer(" + seconds + " seconds) call failed. Called AFTER Start(). Any changes to the maximum amount of allowed time must be made during the Start function in MonoBehaviour.");
+            Debug.LogWarning("SetMaxTimer(" + seconds + " seconds) call failed. Called AFTER Start(). Any changes to the maximum amount of allowed time must be made during the Start function in MonoBehaviour.");
         }
     }
 
     private void Update(){
-        if(!_isInGame){
+        if(!_isInGame && !_endGameLocked){
             _time += Time.deltaTime;
             if (_time > _maxTime){
                 Debug.Log("Reached time limit!");
