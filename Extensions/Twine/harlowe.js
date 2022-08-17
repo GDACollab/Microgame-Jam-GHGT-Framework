85262a69a673efc2424bac3ce3967c5e7d9ba36e
LeftyDark <59773530+LeftyDark@users.noreply.github.com>
2022-08-16 17:36:18 -0700

    Updated Harlowe Extension

--- Extensions/Twine/harlowe.js
+++ Extensions/Twine/harlowe.js
@@ -1,34 +1,87 @@
-// Should be inserted into your Story Javascript:
+// Should be inserted into your Story Javascript
+var isGame;
 try {
-    window.$isGame = window.parent.GameInterface !== undefined && window.parent.GameInterface !== null;
+     if(isGame = parent.GameInterface !== undefined && parent.GameInterface !== null)
+     {isGame = true}
   }
   catch(ex) {
-    window.$isGame = false;
+    isGame = false;
   }
 var time = Date.now();
-window.$maxSeconds = 20;
+var maxSeconds = 15;
+var gameOver = false;
 
-if (window.$isGame) {
-    window.parent.GameInterface.gameStart();
+if (isGame) {
+    parent.GameInterface.gameStart();
 }
 
 function updateVariables() {
-    if (window.$isGame){
-        window.$seconds = window.parent.GameInterface.getTimer();
-        window.$lives = window.parent.GameInterface.getLives();
-        window.$difficulty = window.parent.GameInterface.getDifficulty();
+  	var variables = {};
+    if (isGame){
+        var seconds = parent.GameInterface.getTimer();
+        var lives = parent.GameInterface.getLives();
+        var difficulty = parent.GameInterface.getDifficulty();
+      variables = {seconds, lives, difficulty}
     } else {
-        window.$lives = 3;
-        window.$difficulty = 1;
-        window.$seconds = Math.floor(window.$maxSeconds - ((Date.now() - time) / 1000));
+        lives = 3;
+        difficulty = 1;
+        seconds = Math.floor(maxSeconds - ((Date.now() - time) / 1000));
+      variables = {seconds, lives, difficulty}
     }
-    if (window.$seconds <= 0) {
-        if (window.$isGame) {
+  	Object.entries(variables).forEach(function([key, value]) {
+        State.variables[key] = value;
+    	});
+    if (seconds <= 0) {
+      if (gameOver == false) {
+        if (isGame) {
             window.parent.GameInterface.loseGame();
+          	gameOver = true;
         } else {
             alert("Game Lost!");
+          	gameOver = true;
         }
+      }
     }
 }
+
+const macros = require('macros');
+
+function functionCalls(){
+    var functionNames = {};
+    
+    if (isGame){
+        functionNames = {
+            'WinGame': parent.GameInterface.winGame,
+            'LoseGame': parent.GameInterface.loseGame,
+            'SetMaxTimer': parent.GameInterface.setMaxTimer
+        };
+    } else {
+        functionNames = {
+            'WinGame': () => { if (gameOver == false) {alert("Game Won!"); gameOver = true;}},
+            'LoseGame': () => { if (gameOver == false) {alert("Game Lost!"); gameOver = true;}},
+            'SetMaxTimer': (seconds) => {
+              	if (seconds < 5) {maxSeconds = 5}
+                else if (seconds > 15) {maxSeconds = 15}
+              	else {maxSeconds = seconds};
+            }
+        }
+    }
+  
+      // Based on https://foss.heptapod.net/games/harlowe/-/blob/branch/default/js/macros.js and https://github.com/ChapelR/harlowe-macro-api/blob/master/src/macro.js
+      Object.entries(functionNames).forEach(function([key, value]) {
+        macros.add(key, "Instant", function(_, ...args) {
+          value(args);
+          console.log(args);
+          return {
+              TwineScript_TypeID: "instant",
+            TwineScript_TypeName: "a (" + key + ":) operation",
+            TwineScript_ObjectName: "a (" + key + ":) operation",
+            TwineScript_Unstorable: true,
+            TwineScript_Print: () => ''
+          };
+        }, [parseInt]);
+    });
+}
+functionCalls();
 
 setInterval(updateVariables, 100);
