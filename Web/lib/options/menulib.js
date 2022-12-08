345baa68e4ff8de419ade464e558f0caea41028e
Tyler K <tyly04@gmail.com>
2022-12-08 09:46:30 -0800

    Started on new raycast system

--- Web/lib/options/menulib.js
+++ Web/lib/options/menulib.js
@@ -102,18 +102,53 @@ class MenuVectorField {
         this.#currPos = initialPos;
     }
 
-    raycast(from, direction){
+    // Like a raycast, except this also accounts for rays that are CLOSE enough for the MenuVector field to qualify as "good enough".
+    // So that means that this raycast returns some kind of Selectable, provided that Selectable is either:
+    // Hit by the ray (the ray at some point is within its rectangle).
+    // This will return the selectable that has the closest distance to where the raycast "hits", provided the distance is within the "threshold".
+    // We prioritize elements that are closer to the current element, since this is meant to be used for navigation.
+    raycastEstimate(from, direction, threshold = 100){
         var closestDir = -1;
         var i = -1;
+        
+        // Time to do some cool Calculus ***t *** ********.
+        // Optimization! https://tutorial.math.lamar.edu/Classes/CalcI/Optimization.aspx
+
+        // Direction = (dx/dt, dy/dt).
+        // So direction.y = dy/dt, y = direction.y * t + C_1.
+        // And direction.x = dx/dt, x = direction.x * t + C_2.
+        // C_1 and C_2 are just from.y and from.x respectively.
         this.#selectables.forEach(function(selectable, index){
             if (index !== this.#currPos) {
-                var to = MenuVector.sub(selectable.position, from);
+                // The ray gives us an equation that we can solve for:
+                
+                var to = MenuVector.sub(selectable.center, from);
                 var dot = direction.dot(MenuVector.normalized(to));
                 if (dot > 0.5) {
-                    var dirClose = MenuVector.scalarMul(direction, to.length).add(from);
-                    var dist = dirClose.dist(selectable.position);
-                    if (dist <= 100 && (dist < closestDir || closestDir === -1)) {
-                        closestDir = dist;
+                    // Now we construct a function that, given some value t, gives us the distance to the center of the rect.
+                    // If you look at the problem on graph paper, you'll see that (For STRAIGHT LINES) the closest distance to any rect is also the closest point to that rect's center.
+                    // Then we want to find the global minima of that function.
+
+                    // sqrt( (direction.y * t + C_1 - center.y, 2)^2 + (direction.x * t + C_2 - center.x)^2 )
+                    // Is the function that gives us the distance between any given point on our line constructed from the ray and the center.
+                    // Taking the derivative with respect to t, we get
+                    // (direction.y * (direction.y * t + C_1 - center.y) + direction.x * (direction.x * t + C_2 - center.x))/the whole sqrt from before, I'm not gonna write it all out.
+                    // We want the smallest value, so we need to find where the values of dt are zero. Setting the LHS to zero, we get:
+                    // 0 = direction.y * (direction.y * t + C_1 - center.y) + direction.x * (direction.x * t + C_2 - center.x).
+                    // And there's only one value of t, which is our global minima:
+                    // t = (direction.y * (center.y - C_1) + direction.x * (center.x - C_2))/(direction.x^2 + direction.y^2).
+                    var t = (direction.y * (selectable.center.y - from.y) + direction.x * (selectable.center.x - from.x))/(Math.pow(direction.x, 2) + Math.pow(direction.y, 2));
+                    // And so we get the x and y from our t-value:
+                    var dirClose = new MenuVector(direction.x * t + from.x, direction.y * t + from.y);
+                    // And now we just find the distance, and see if it's closer than the other distances we've calculated.
+                    var dist = dirClose.dist(selectable.center);
+
+                    var selectFullPos = selectable.fullPos;
+                    var withinRectBounds = dirClose.x >= selectFullPos.x && dirClose.x <= selectFullPos.x + selectable.element.offsetWidth && dirClose.y >= selectFullPos.y && dirClose.y <= selectFullPos.y + selectable.element.offsetHeight;
+
+                    var actualElementDist = from.dist(selectable.center);
+                    if ((dist <= threshold || withinRectBounds) && (actualElementDist < closestDir || closestDir === -1)) {
+                        closestDir = actualElementDist;
                         i = index;
                     } 
                 }
@@ -126,7 +161,7 @@ class MenuVectorField {
         // debugger;
         var pick = -1;
         if (direction.x !== 0 || direction.y !== 0) {
-            pick = this.raycast(this.#selectables[this.#currPos].position, direction);
+            pick = this.raycastEstimate(this.#selectables[this.#currPos].position, direction);
 
             if (pick  !== -1) {
                 this.#currPos = pick;
@@ -145,6 +180,7 @@ class MenuVectorField {
 }
 
 class Selectable {
+    #center;
     constructor(baseElement, existingPosition = null) {
         this.element = baseElement;
         if (existingPosition !== null) {
@@ -152,6 +188,29 @@ class Selectable {
         } else {
             this.findPosition();
         }
+        this.#center = new MenuVector(this.position.x + this.element.offsetWidth/2, this.position.y + this.element.offsetHeight/2);
+    }
+
+    get fullPos() {
+        var toReturn = new MenuVector(this.position);
+        var translateMatrix = window.getComputedStyle(this.element).transform.replace(")", "").split(",");
+        var left = parseFloat(translateMatrix[4]);
+        var top = parseFloat( translateMatrix[5]);
+        if (!isNaN(left) && !isNaN(top)){
+            toReturn.add(left, top);
+        }
+        return toReturn;
+    }
+
+    get center() {  
+        var toReturn = new MenuVector(this.#center);
+        var translateMatrix = window.getComputedStyle(this.element).transform.replace(")", "").split(",");
+        var left = parseFloat(translateMatrix[4]);
+        var top = parseFloat( translateMatrix[5]);
+        if (!isNaN(left) && !isNaN(top)){
+            toReturn.add(left, top);
+        }
+        return toReturn;
     }
 
     // Based on the stuff I did for the twine extension.
